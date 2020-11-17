4672a5f031a7f0803da6c5674008c5a62528d663
tparchman <65676283+tparchman@users.noreply.github.com>
2020-11-17 14:25:17 -0800

    update

---
+++ analysis_workflow_varcal.md
@@ -0,0 +1,86 @@
+### Notes on cleaning Attenuata data 8/20
+
+
+`NOTE`: Here we need to clean two lanes of NovaSeq data. The first (sample-1_S1_L001_R1_001.fastq.gz) has PHHA, ACTH, CHDO, and PIAT. The second (sample-2_S2_L002_R1_001.fastq.gz) has POSE, PIAT, and some quadrus. We will need to clean and parse both to get all of the attenuata samples into working. 
+
+`NOTE`: Most analyses will be completed at 
+
+### This file contains code and notes for
+1) cleaning contaminants using tapioca
+2) parsing barcodes
+3) splitting fastqs 
+4) 
+5) calling variants
+6) filtering
+7) entropy for genotype probabilities.
+
+## Cleaning contaminants
+
+Being executed on ponderosa using tapioca pipeline. Commands in bash script, executed as below (11/17/20).
+
+    $ module load fqutils/0.4.1
+    $ module load bowtie2/2.2.5
+    
+    $ bash cleaning_bash.sh &
+
+# DONE TO HERE, BELOW IS PLACEHOLDER FROM TPOD
+
+After tpodura.clean.fastq has been produced, clean out duplicate raw data:
+
+    $ rm -rf L2-timema_S2_L002_R1_001.fastq
+ 
+Number of reads **before** cleaning:
+
+    $ grep -c "^@" L2-timema_S2_L002_R1_001.fastq > number_of_rawreads.txt
+    $ less number_of_rawreads.txt
+    # 2,113,546,977
+    
+Number of reads **after** cleaning:
+
+    $ grep -c "^@" tpodura.clean.fastq > number_of_cleanreads.txt
+    $ less number_of_cleanreads.txt
+    # 1,675,695,246
+        
+## Barcode parsing:
+
+Barcode keyfile is `/working/parchman/tpodura/timema_podura_bcode.csv`
+  
+    $ perl parse_barcodes768.pl final_timema3_Piper1_bcode.csv tpodura.clean.fastq A00 &
+
+`NOTE`: the A00 object is the code that identifies the sequencer (first three characters after the @ in the fastq identifier).
+
+    $ less parsereport_tpodura.clean.fastq
+    #Good mids count: 1617562664
+    #Bad mids count: 58132389
+    #Number of seqs with potential MSE adapter in seq: 305112
+    #Seqs that were too short after removing MSE and beyond: 193
+          
+Cleaning up the directory:
+
+    $ rm tpodura.clean.fastq
+    $ rm miderrors_tpodura.clean.fastq
+    $ rm parsereport_tpodura.clean.fastq
+
+### Alignment to *T. cristinae* genome and variant calling.
+New software needs to be installed on ponderosa, including:
+- bwa 0.7.17-r1188 (https://github.com/lh3/bwa/releases)
+- bcftools 1.9 (under https://sourceforge.net/projects/samtools/files/samtools/1.9/)
+- samtools 1.10 (under https://sourceforge.net/projects/samtools/files/samtools/1.10/)
+## mapping with `bwa`
+
+
+## alignment with bwa 0.7.17-r1188
+## assumes genome = genome.fasta and the sample is tpod1
+
+bwa mem -k 20 -w 100 -r 1.3 -T 30 -R @RG        ID:tpod1        PL:ILLUMINA     LB:tpod1        SM:tpod1 genome.fasta tpod1.fastq > aln_tpod1.sam 2> tpod1.log
+
+## standard sam to bam, sorting and indexing; code not included, same as it has always been
+## at present using samtools 1.10 and bcftools 1.9
+
+## variant calling with bcftools 1.9
+## bams is a text file with all of the sorted bam files listed, one per line
+bcftools mpileup -C 50 -d 250 -f genome.fasta -q 30 -Q 20 -I -b bams -O b -o tpod.bcf
+
+## sometimes I use the -c option, sometimes not, I have mixed feelings, probably would use it
+## for podura
+bcftools call -v -c -f GQ -p 0.01 -P 0.001 -O v -o tpod.vcf tpod.bcf
--- cleaning_bash.sh
+++ cleaning_bash.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 
 ##########################################################################################
-## T.podura cleaning 1
+## UTGSAF 11_20 cleaning 1 (PHHA, ACTH, CHDO, PIAT)
 ##########################################################################################
 
-/working/jahner/tapioca/src/tap_contam_analysis --db  /archive/parchman_lab/rawdata_to_backup/contaminants/illumina_oligos --pct 20 /working/parchman/tpodura/L2-timema_S2_L002_R1_001.fastq > tpodura.readstofilter.ill.txt 
+/working/jahner/tapioca/src/tap_contam_analysis --db  /archive/parchman_lab/rawdata_to_backup/contaminants/illumina_oligos --pct 20 /mnt/UTGSAF_11_20/sample-1_S1_L001_R1_001.fastq > S1_11_20.readstofilter.ill.txt 
+
+echo "Illumina filtering done for lane 1"
+
+/working/jahner/tapioca/src/tap_contam_analysis --db /archive/parchman_lab/rawdata_to_backup/contaminants/phix174 --pct 80 /mnt/UTGSAF_11_20/sample-1_S1_L001_R1_001.fastq  > S1_11_20.readstofilter.phix.txt 
+
+echo "PhiX filtering done for lane 1"
+
+
+/working/jahner/tapioca/src/tap_contam_analysis --db  /archive/parchman_lab/rawdata_to_backup/contaminants/ecoli-k-12 --pct 80 /mnt/UTGSAF_11_20/sample-1_S1_L001_R1_001.fastq  > S1_11_20.readstofilter.ecoli.txt
+
+echo "ecoli filtering done for lane 1"
+
+
+cat /mnt/UTGSAF_11_20/sample-1_S1_L001_R1_001.fastq | fqu_cull -r S1_11_20.readstofilter.ill.txt S1_11_20.readstofilter.phix.txt S1_11_20.readstofilter.ecoli.txt > S1_11_20.clean.fastq
+
+echo "Clean copy of lane 1 done"
+
+##########################################################################################
+## UTGSAF 11_20 cleaning 2 (POSE, Quadrus, PIAT)
+##########################################################################################
+
+/working/jahner/tapioca/src/tap_contam_analysis --db  /archive/parchman_lab/rawdata_to_backup/contaminants/illumina_oligos --pct 20 /mnt/UTGSAF_11_20/sample-2_S2_L002_R1_001.fastq > S2_11_20.readstofilter.ill.txt 
 
 echo "Illumina filtering done for lane 2"
 
-/working/jahner/tapioca/src/tap_contam_analysis --db /archive/parchman_lab/rawdata_to_backup/contaminants/phix174 --pct 80 /working/parchman/tpodura/L2-timema_S2_L002_R1_001.fastq > tpodura.readstofilter.phix.txt 
+/working/jahner/tapioca/src/tap_contam_analysis --db /archive/parchman_lab/rawdata_to_backup/contaminants/phix174 --pct 80 /mnt/UTGSAF_11_20/sample-2_S2_L002_R1_001.fastq  > S2_11_20.readstofilter.phix.txt 
 
 echo "PhiX filtering done for lane 2"
 
 
-/working/jahner/tapioca/src/tap_contam_analysis --db  /archive/parchman_lab/rawdata_to_backup/contaminants/ecoli-k-12 --pct 80 /working/parchman/tpodura/L2-timema_S2_L002_R1_001.fastq > tpodura.readstofilter.ecoli.txt
+/working/jahner/tapioca/src/tap_contam_analysis --db  /archive/parchman_lab/rawdata_to_backup/contaminants/ecoli-k-12 --pct 80 /mnt/UTGSAF_11_20/sample-2_S2_L002_R1_001.fastq  > S2_11_20.readstofilter.ecoli.txt
 
 echo "ecoli filtering done for lane 2"
 
 
-cat /working/parchman/tpodura/L2-timema_S2_L002_R1_001.fastq | fqu_cull -r tpodura.readstofilter.ill.txt tpodura.readstofilter.phix.txt tpodura.readstofilter.ecoli.txt > tpodura.clean.fastq
+cat /mnt/UTGSAF_11_20/sample-2_S2_L002_R1_001.fastq | fqu_cull -r S2_11_20.readstofilter.ill.txt S2_11_20.readstofilter.phix.txt S2_11_20.readstofilter.ecoli.txt > S2_11_20.clean.fastq
 
 echo "Clean copy of lane 2 done"
 
